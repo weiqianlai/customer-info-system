4065fe1acbf83970a9230c469a46257a5f4fa0b0
weiqianlai <2314604007@qq.com>
2019-03-02 19:55:04 +0800

    2019-3-2
    1，完成了客户信息显示错位的问题；
    2，完成了admin的界面，逻辑代码（js)还没有写。

--- customer_Info/pages/admin/admin-customerinfo/admin-customerinfo.js
+++ customer_Info/pages/admin/admin-customerinfo/admin-customerinfo.js
@@ -5,62 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    post: [{
+      name: '韦乾来',
+      sex: '男',
+      tel_no: '18224995161'
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
+  },
+  onfollow_up: function(e) {
+    var id = e.target.dataset.id;
+    var type = wx.getStorageSync("user-type");
+    console.log("user-file", " onUpdate==> 客户信息记录的id=" + id + "  type=" + type);
+    wx.redirectTo({
+      url: 'customerinfo-detail/customerinfo-detail?id=' + id + "&&type=" + type,
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- customer_Info/pages/admin/index/index.js
+++ customer_Info/pages/admin/index/index.js
@@ -7,62 +7,62 @@ Page({
   data: {
     routers: [{
       name: '高升专',
-      url: '../admin-customerinfo/admin-customerinfo',
+      url: '../admin-customerinfo/admin-customerinfo?type=1001',
       icon: '../../../images/nav-icon/gsz.jpg',
       id: "001"
     }, {
       name: '专升本',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1002',
       icon: '../../../images/nav-icon/zsb.jpg',
       id: "002"
     }, {
       name: '非全日制研究生',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1003',
       icon: '../../../images/nav-icon/fqrzyjs.jpg',
       id: "003"
     }, {
       name: '普通话培训',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1004',
       icon: '../../../images/nav-icon/pth.jpg',
       id: "004"
     }, {
       name: '教师资格证',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1005',
       icon: '../../../images/nav-icon/jszg.jpg',
       id: "005"
     }, {
       name: '建筑工程',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1006',
       icon: '../../../images/nav-icon/jzgc.jpg',
       id: "006"
     }, {
       name: '职业资格',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1007',
       icon: '../../../images/nav-icon/zyzg.jpg',
       id: "007"
     }, {
       name: '医药卫生',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1008',
       icon: '../../../images/nav-icon/yyws.jpg',
       id: "008"
     }, {
       name: '外语少儿',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1009',
       icon: '../../../images/nav-icon/wysr.png',
       id: "009"
     }, {
       name: '财会经济',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1010',
       icon: '../../../images/nav-icon/ckjj.jpg',
       id: "010"
     }, {
       name: '欢迎您的加入',
-      url: '../index/index',
+      url: '',
       icon: ' ',
       id: "00x"
     }, {
       name: '法院书记员',
-        url: '../admin-customerinfo/admin-customerinfo',
+        url: '../admin-customerinfo/admin-customerinfo?type=1011',
       icon: '../../../images/nav-icon/fysjy.jpg',
       id: "00x"
     }]
